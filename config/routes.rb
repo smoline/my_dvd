f693dbafeffed975c6c000073d18c8c885f8447d
Sherilyn Moline <smoline@gmail.com>
2017-05-03 22:01:13 -0400

    Landing page is now default, login page is customized

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
 
   get 'pages/landing'
-  root 'pages#landing'
+
+  devise_scope :user do
+    root 'pages#landing'
+  end
+
   resources :dvds
 
 end
