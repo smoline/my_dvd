7c379644ace059b3aa2fc555f09adc984f6df185
Sherilyn Moline <smoline@gmail.com>
2017-05-06 14:59:57 -0400

    facebook, google, linkedin, github logins working

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,53 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+  def self.from_omniauth(auth)
+    user = User.find_by('email = ?', auth['info']['email'])
+    if user.blank?
+      user = User.new(
+        {
+          provider: auth.provider,
+          uid: auth.uid,
+          email: auth.info.email,
+          password: Devise.friendly_token[0,20]
+        })
+      user.save!
+    end
+    user
+  end
+
+  # def self.from_omniauth(authentication_data)
+  #   if User.where(provider: authentication_data['provider'],
+  #                     uid: authentication_data['uid']).exists?
+  #     user = User.where(provider: authentication_data['provider'],
+  #                     uid: authentication_data['uid']).first
+  #   else
+  #     user = User.where(provider: authentication_data['provider'],
+  #                       uid: authentication_data['uid']).create
+  #
+  #     Rails.logger.debug "The user is #{user.inspect}"
+  #     Rails.logger.debug "The authinfo is #{authentication_data.info}"
+  #
+  #     user.name         = authentication_data.info.name
+  #     user.nickname     = authentication_data.info.nickname
+  #     user.access_token = authentication_data.info.access_token
+  #     user.profile_url  = authentication_data.info.urls
+  #     user.email        = authentication_data.info.email
+  #
+  #     user.save!
+  #   end
+  #
+  #   Rails.logger.debug "After saving, the user is #{user.inspect}"
+  #
+  #   return user
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  devise_scope :user do
+    get "/users/auth/facebook/callback" => "users/omniauth_callbacks#facebook"
+    get "/users/auth/google/callback" => "users/omniauth_callbacks#google"
+    get "/users/auth/linkedin/callback" => "users/omniauth_callbacks#linkedin"
+    get "/users/auth/github/callback" => "users/omniauth_callbacks#github"
+  end
+
   get 'pages/landing'
 
   root 'pages#landing'
